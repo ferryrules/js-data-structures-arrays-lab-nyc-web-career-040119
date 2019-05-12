bb7b710b2a6e1671dd712d50a1db4b8c6887c464
ferryrules <ferris@ferryrules.com>
2019-05-12 16:49:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(driver) {
+  drivers.push(driver)
+}
+
+function destructivelyPrependDriver(driver) {
+  drivers.unshift(driver)
+}
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop()
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift()
+}
+
+function appendDriver(driver) {
+  var arr = []
+  arr.push(driver)
+  return drivers.concat(arr)
+}
+
+function prependDriver(driver) {
+  var arr = []
+  arr.push(driver)
+  return arr.concat(drivers)
+}
+
+function removeLastDriver() {
+  var arr = drivers.slice(0,-1)
+  return arr
+}
+
+function removeFirstDriver() {
+  var arr = drivers.slice(1);
+  return arr
+}
